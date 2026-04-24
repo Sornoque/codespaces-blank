67d84cfdf882ca30f68283a1db7ccc56b8002264
Sornoque <laurassf7@gmail.com>
2026-04-24 21:33:00 +0000

    update

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
-const navbarToggle = document.querySelector(".navbar-toggle");
-const navbarMenu = document.querySelector('.navbar-menu')
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
 
 navbarToggle.addEventListener('click', () => {
-    navbarToggle.classList.toggle('active');
-    navbarMenu.classList.toggle('active');
-})
+  navbarMenu.classList.toggle('active');
+});
