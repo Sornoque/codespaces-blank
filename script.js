aad4c07d94166cd5acce37f1a7b0d9c95bb5d5e1
Sornoque <laurassf7@gmail.com>
2026-04-29 05:39:56 +0000

    carousel almost working

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const navbarMenu = document.querySelector('.navbar-menu');
 navbarToggle.addEventListener('click', () => {
   navbarMenu.classList.toggle('active');
 });
+
+const carousel = document.querySelector('.carousel');
+const dots = document.querySelector('.dot');
+
+let index = 0;
+
+function updateCarousel(){
+  carousel.scrollLeft = index * 320;
+  dots.forEach(dot => { dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
+
+document.querySelector('right').onclick = () => {
+  if (index < 2) {
+    index++;
+    updateCarousel();
+  }
+}
+
+document.querySelector('left').onclick = () => {
+  if (index < 0) {
+    index--;
+    updateCarousel();
+  }
+}
